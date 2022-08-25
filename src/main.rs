a2a1ef7b04a2d3203d1da0c63a55db98b18fd6dc
cutsea110 <cutsea110@gmail.com>
2022-08-26 07:16:09 +0900

    catch the error

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,31 @@ fn hello() {
     println!("Hello World!");
 }
 
-fn main() {
+fn my_func1() {
     spawn(hello).join();
 
     let h = || println!("Hello World!");
     spawn(h).join();
+}
+
+fn my_func2() {
+    let v = 10;
+    let f = move || v * 2;
+
+    let result = spawn(f).join();
+    println!("result = {:?}", result);
+
+    match spawn(|| panic!("I'm paniced!")).join() {
+        Ok(_) => {
+            println!("successed");
+        }
+        Err(a) => {
+            let s = a.downcast_ref::<&str>();
+            println!("failed: {:?}", s)
+        }
+    }
+}
+
+fn main() {
+    my_func2();
 }
